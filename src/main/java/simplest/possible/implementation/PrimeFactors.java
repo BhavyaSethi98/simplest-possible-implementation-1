5653a7d94003329382a75196a14fc344cb514ade
Bhavya Sethi <bhavyasethi@Bhavyas-MacBook-Pro.local>
2021-01-04 17:30:02 +0530

    Completed the first test case implementation

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,12 @@
+package simplest.possible.implementation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactors {
+    public List<Integer> of(int number) {
+        ArrayList<Integer> primeFactorsResult = new ArrayList<>();
+        return primeFactorsResult;
+
+    }
+}
